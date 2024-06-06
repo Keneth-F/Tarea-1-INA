d0bbd761cb76d90b33256fc3593b23ad84101856
Keneth-F <kenethfg123@gmail.com>
2024-06-06 10:50:13 -0600

    Agregar archivos de proyecto.

---
+++ Tarea-1-INA/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Tarea_1_INA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.CbNums1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.CbNewNums2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CbNums2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.LblName = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.CbNewNums3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CbNums3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(861, 534);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.CbNums1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(853, 508);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Numeros 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // CbNums1
+            // 
+            this.CbNums1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbNums1.Location = new System.Drawing.Point(81, 6);
+            this.CbNums1.Margin = new System.Windows.Forms.Padding(2);
+            this.CbNums1.Name = "CbNums1";
+            this.CbNums1.Size = new System.Drawing.Size(92, 21);
+            this.CbNums1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Array :";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.CbNewNums2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.CbNums2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(853, 508);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Numeros 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // CbNewNums2
+            // 
+            this.CbNewNums2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbNewNums2.Location = new System.Drawing.Point(81, 34);
+            this.CbNewNums2.Margin = new System.Windows.Forms.Padding(2);
+            this.CbNewNums2.Name = "CbNewNums2";
+            this.CbNewNums2.Size = new System.Drawing.Size(92, 21);
+            this.CbNewNums2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Array :";
+            // 
+            // CbNums2
+            // 
+            this.CbNums2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbNums2.Location = new System.Drawing.Point(146, 6);
+            this.CbNums2.Margin = new System.Windows.Forms.Padding(2);
+            this.CbNums2.Name = "CbNums2";
+            this.CbNums2.Size = new System.Drawing.Size(92, 21);
+            this.CbNums2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 3);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Array Original :";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.LblName);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(853, 508);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Nombres";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Location = new System.Drawing.Point(8, 0);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(198, 22);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Nombre en posicion -1";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.CbNewNums3);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Controls.Add(this.CbNums3);
+            this.tabPage4.Controls.Add(this.label5);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(853, 508);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Numeros 3";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // CbNewNums3
+            // 
+            this.CbNewNums3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbNewNums3.Location = new System.Drawing.Point(81, 31);
+            this.CbNewNums3.Margin = new System.Windows.Forms.Padding(2);
+            this.CbNewNums3.Name = "CbNewNums3";
+            this.CbNewNums3.Size = new System.Drawing.Size(92, 21);
+            this.CbNewNums3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 28);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Array :";
+            // 
+            // CbNums3
+            // 
+            this.CbNums3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CbNums3.Location = new System.Drawing.Point(146, 3);
+            this.CbNums3.Margin = new System.Windows.Forms.Padding(2);
+            this.CbNums3.Name = "CbNums3";
+            this.CbNums3.Size = new System.Drawing.Size(92, 21);
+            this.CbNums3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Array Original :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 534);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ComboBox CbNums1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox CbNums2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CbNewNums2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ComboBox CbNewNums3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CbNums3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Tarea-1-INA/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_1_INA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("1. Numeros: ");
+            int[] numeros = { 10, 20, 30, 40, 50 };
+            foreach (int i in numeros)
+            {
+                CbNums1.Items.Add(i);
+            }
+            Console.Write(numeros.Length);
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("2. Numeros: ");
+            numeros = new int[] { 5, 10, 15, 20, 25 };
+            foreach (int i in numeros)
+            {
+                CbNums2.Items.Add(i);
+                Console.Write(i + " ");
+            }
+            Console.Write(" | ");
+            numeros[2] = 100;
+            foreach (int i in numeros)
+            {
+                CbNewNums2.Items.Add(i);
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("3. Nombres: ");
+            string[] nombres = { "Ana", "Luis", "Pedro", "Marta" };
+            int index = Array.IndexOf(nombres, "Pedro");
+            Console.WriteLine(index);
+            if (index != -1)
+            {
+                LblName.Text = $"Pedro en posicion {index}";
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("4. Numeros: ");
+            numeros = new int[] { 50, 40, 30, 20, 10 };
+            foreach (int i in numeros)
+            {
+                CbNums3.Items.Add(i);
+                Console.Write(i + " ");
+            }
+            Console.Write(" | ");
+            Array.Sort(numeros);
+            foreach (int i in numeros)
+            {
+                CbNewNums3.Items.Add(i);
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("5. Numeros: ");
+            numeros = new int[] { 1, 2, 3, 4, 5 };
+            Array.Reverse(numeros);
+            foreach (int i in numeros)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("6. Numeros: ");
+            Array.Clear(numeros, 1, 3);
+            foreach (int i in numeros)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("7. Numeros: ");
+            int[] origen = { 1, 2, 3, 4, 5 };
+            int[] destino = new int[origen.Length];
+            Array.Copy(origen, destino, origen.Length);
+            foreach (int i in destino)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("8. Numeros: ");
+            numeros = new int[] { 10, 20, 30, 40, 50 };
+            Console.WriteLine(Array.Find(numeros, n => n > 20));
+            Console.WriteLine("------------------");
+            Console.WriteLine("9. Numeros: ");
+            int[] valores = { 1, 3, 5, 7, 9 };
+            Console.WriteLine(Array.Exists(valores, n => n > 6));
+            Console.WriteLine("------------------");
+            Console.WriteLine("10. Numeros: ");
+            numeros = new int[] { 2, 4, 6, 8, 10 };
+            Console.WriteLine(Array.Find(numeros, n => n % 2 == 0));
+            Console.WriteLine("------------------");
+            Console.WriteLine("11. Numeros: ");
+            string[] frutas = { "manzana", "banana", "naranja", "kiwi" };
+            Console.WriteLine(Array.IndexOf(frutas, "kiwi"));
+            Console.WriteLine("------------------");
+            Console.WriteLine("12. Numeros: ");
+            numeros = new int[] { 15, 25, 35, 45, 55, 66 };
+            Array.Clear(numeros, 2, 4);
+            foreach (int i in numeros)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("13. Numeros: ");
+            int[] datos = { 5, 10, 15, 20, 25 };
+            int[] SubDatos = new int[3];
+            Array.Copy(datos, SubDatos, 3);
+            foreach (int i in SubDatos)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("14. Numeros: ");
+            valores = new int[] { 3, 6, 9, 12, 15 };
+            Array.Reverse(valores);
+            foreach (int i in valores)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+            Console.WriteLine("15. Numeros: ");
+            numeros = new int[] { 5, 10, 15, 20, 25 };
+            Console.WriteLine(Array.Exists(numeros, n => n == 10));
+            Console.WriteLine("------------------");
+            Console.WriteLine("16. Numeros: ");
+            Array.Sort(numeros);
+            Console.WriteLine(numeros[0]);
+            Console.WriteLine("------------------");
+            Console.WriteLine("17. Numeros: ");
+            Array.Sort(numeros);
+            Console.WriteLine(numeros[numeros.Length - 1]);
+            Console.WriteLine("------------------");
+            Console.WriteLine("18. Numeros: ");
+            int sum = 0;
+            foreach (int item in numeros)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
